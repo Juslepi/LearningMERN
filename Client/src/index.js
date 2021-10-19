2b96112c717c6ccc8a8f1723fa0c1d24121f4514
unknown <juslepi@gmail.com>
2021-10-19 03:35:06 +0300

    cleaned todos

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,19 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
-// TODO - Animated navbar expand
-// TODO - Context and Dark theme
-// TODO - Link component
-// TODO - Guestbook
-// TODO - MongoDB CRUD
-// TODO - REFACTOR SERVER CODE!!! :--D
-// TODO - Authentication
-
-
-// Read more
-// Async await
-// Promise .then
-
 ReactDOM.render(
   <React.StrictMode>
     <Router>
